28240667e8be06137e80935a19bde67d57f1685d
Jose Valle <jose_vr_th98@hotmail.com>
2026-02-09 18:40:14 -0600

    m

--- api/procesar.js
+++ api/procesar.js
@@ -1,15 +1,7 @@
-export default function handler(req, res){
-    const nombre = req.query.nombre || "anonimo";
+export default function handler(req, res) {
+  const nombre = req.query.nombre || "anónimo";
 
-    if(nombre == "error"){
-        res.status(400).json({
-            resultado: "Error: no se puede usar 'error' como nombre",
-            timestamp: new Date().toISOString()
-        });
-    }else{
-        res.status(200).json({
-            resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
-            timestamp: new Date().toISOString()
-        })
-    }
+  res.status(200).json({
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
+  });
 }
