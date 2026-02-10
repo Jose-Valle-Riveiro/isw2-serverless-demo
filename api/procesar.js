66e96c0cd9d2c919875a8130090d83e43298b461
Jose Valle <jose_vr_th98@hotmail.com>
2026-02-09 19:19:22 -0600

    Added tests for error name

--- api/procesar.js
+++ api/procesar.js
@@ -1,8 +1,15 @@
-export default function handler(req, res) {
-  const nombre = req.query.nombre || "anónimo";
+export default function handler(req, res){
+    const nombre = req.query.nombre || "anonimo";
 
-  res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
-    longitud: nombre.length
-  });
+    if(nombre == "error"){
+        res.status(400).json({
+            resultado: "Error: no se puede usar 'error' como nombre",
+            longitud: nombre.length
+        });
+    }else{
+        res.status(200).json({
+            resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
+            longitud: nombre.length
+        })
+    }
 }
--- test/procesar.test.js
+++ test/procesar.test.js
@@ -49,3 +49,29 @@ test("procesar maneja nombre ausente", () => {
   assert.equal(res.statusCode, 200);
   assert.ok(res.body.resultado.includes("ANÓNIMO"));
 });
+
+test("Verificar que no se acepte error como nombre", () => {
+    const req = {query: {nombre:"error"}};
+
+    const res = {
+        statusCode: null,
+        body: null,
+        status(code){
+            this.statusCode = code;
+            return this;
+        },
+        json(payload){
+            this.body = payload
+            return this;
+        }
+    };
+
+    handler(req, res);
+
+    assert.equal(res.statusCode, 400);
+    assert.deepEqual(res.body, {
+        resultado: "Error: no se puede usar 'error' como nombre",
+        longitud: 5
+    });
+
+});
